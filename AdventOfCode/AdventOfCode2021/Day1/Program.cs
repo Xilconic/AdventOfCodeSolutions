f44b43bbb3bbe712b0ca68d62e97a36589e899ca
xilconic <xilconic@gmail.com>
2021-12-02 20:34:44 +0100

    Refactor AoC2021_Day1 to support part 2 of the puzzle

--- AdventOfCode/AdventOfCode2021/Day1/Program.cs
+++ AdventOfCode/AdventOfCode2021/Day1/Program.cs
@@ -7,17 +7,43 @@ namespace AdventOfCode2021.Day1
     {
         public static void Main(string[] args)
         {
-            if (args.Length != 1)
+            if (args.Length != 2)
             {
                 throw new ArgumentException(
-                    "Program must be supplied exactly 1 argument, being the puzzle input file.",
+                    "Program must be supplied exactly 2 arguments; First being the puzzle input file, second being the part number in the range [1,2].",
                     nameof(args));
             }
 
+            switch (ParsePuzzleSolvingMode(args[1]))
+            {
+                case PuzzleSolvingMode.Part1:
+                    SolvePuzzlePart1(args[0]);
+                    break;
+                case PuzzleSolvingMode.Part2:
+                    SolvePuzzlePart2(args[0]);
+                    break;
+            }
+        }
+
+        private static PuzzleSolvingMode ParsePuzzleSolvingMode(string puzzlePartNumber)
+        {
+            if (!int.TryParse(puzzlePartNumber, out int number))
+                throw new ArgumentException($"2nd argument only supports values in the range [1,2]. Received {puzzlePartNumber}", "args[1]");
+            
+            return number switch
+            {
+                1 => PuzzleSolvingMode.Part1,
+                2 => PuzzleSolvingMode.Part2,
+                _ => throw new ArgumentException($"2nd argument only supports values in the range [1,2]. Received {number}", "args[1]")
+            };
+        }
+
+        private static void SolvePuzzlePart1(string filename)
+        {
             bool isFirstMeasurement = true;
             int previousDepthMeasurement = default;
             int numberOfTimesCurrentMeasurementWasHigherThanPrevious = 0;
-            foreach (string line in File.ReadLines(args[0]))
+            foreach (string line in File.ReadLines(filename))
             {
                 if (!int.TryParse(line, out int depthMeasurement))
                 {
@@ -41,6 +67,11 @@ namespace AdventOfCode2021.Day1
             
             Console.WriteLine("How many measurements are larger than the previous measurement?");
             Console.WriteLine(numberOfTimesCurrentMeasurementWasHigherThanPrevious);
+        }
+        
+        private static void SolvePuzzlePart2(string s)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ AdventOfCode/AdventOfCode2021/Day1/PuzzleSolvingMode.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2021.Day1
+{
+    public enum PuzzleSolvingMode
+    {
+        Part1,
+        Part2
+    }
+}
