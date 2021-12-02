e96f7de4d6816a5ec21e0def15691764f854c19e
xilconic <xilconic@gmail.com>
2021-12-02 20:16:28 +0100

    Day1 - part 1 finished correctly

---
+++ AdventOfCode/AdventOfCode2021/Day1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2021.Day1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new ArgumentException(
+                    "Program must be supplied exactly 1 argument, being the puzzle input file.",
+                    nameof(args));
+            }
+
+            bool isFirstMeasurement = true;
+            int previousDepthMeasurement = default;
+            int numberOfTimesCurrentMeasurementWasHigherThanPrevious = 0;
+            foreach (string line in File.ReadLines(args[0]))
+            {
+                if (!int.TryParse(line, out int depthMeasurement))
+                {
+                    throw new FormatException($"Puzzle input should have a single integer value per line in the file. Instead, read unexpected {line}.");
+                }
+
+                if (isFirstMeasurement)
+                {
+                    isFirstMeasurement = false;
+                }
+                else
+                {
+                    if (depthMeasurement > previousDepthMeasurement)
+                    {
+                        numberOfTimesCurrentMeasurementWasHigherThanPrevious += 1;
+                    }
+                }
+
+                previousDepthMeasurement = depthMeasurement;
+            }
+            
+            Console.WriteLine("How many measurements are larger than the previous measurement?");
+            Console.WriteLine(numberOfTimesCurrentMeasurementWasHigherThanPrevious);
+        }
+    }
+}
