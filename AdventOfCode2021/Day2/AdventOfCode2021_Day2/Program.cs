5e4e206c32be0a9413c59c4e49130315d5b99f58
xilconic <xilconic@gmail.com>
2021-12-03 19:50:06 +0100

    Solution for Day2 - part1 of 2021 puzzle

---
+++ AdventOfCode2021/Day2/AdventOfCode2021_Day2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2021_Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new ArgumentException(
+                    "Program must be supplied exactly 1 arguments, being the puzzle input file.",
+                    nameof(args));
+            }
+
+            var submarine = new Submarine();
+
+            foreach (string submarineControlInstruction in File.ReadLines(args[0]))
+            {
+                Match match = Regex.Match(submarineControlInstruction, @"(?<command>\w+)\s(?<distance>\d+)");
+                string command = match.Groups["command"].Value;
+                string distanceAsText = match.Groups["distance"].Value;
+                int distance = int.Parse(distanceAsText);
+                
+                switch (command)
+                {
+                    case "forward":
+                        submarine.GoForward(distance);
+                        break;
+                    case "down":
+                        submarine.GoDeeper(distance);
+                        break;
+                    case "up":
+                        submarine.GoHigher(distance);
+                        break;
+                }
+            }
+            
+            Console.WriteLine("What do you get if you multiply your final horizontal position by your final depth?");
+            Console.WriteLine(submarine.GetDepthTimesForwardDistance());
+        }
+    }
+}
---
+++ AdventOfCode2021/Day2/AdventOfCode2021_Day2/Submarine.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode2021_Day2
+{
+    internal class Submarine
+    {
+        private int _horizontalPosition = 0;
+        private int _depth = 0;
+        
+        public void GoForward(int distance)
+        {
+            _horizontalPosition += distance;
+        }
+
+        public void GoDeeper(int distance)
+        {
+            _depth += distance;
+        }
+
+        public void GoHigher(int distance)
+        {
+            _depth -= distance;
+        }
+
+        public long GetDepthTimesForwardDistance() => _depth * _horizontalPosition;
+    }
+}
