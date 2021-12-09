d5a465e51fbb504de1be1b5f2f3d444ff9a3d8d6
xilconic <xilconic@gmail.com>
2021-12-09 19:37:38 +0100

    Solution for Day7 - part1 of 2021 puzzle

---
+++ AdventOfCode2021/Day7/AdventOfCode2021_Day7/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021_Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                throw new ArgumentException(
+                    "Program must be supplied exactly 2 arguments; First being the puzzle input file, second being the part number in the range [1,2].",
+                    nameof(args));
+            }
+
+            string filename = args[0];
+            switch (ParsePuzzleSolvingMode(args[1]))
+            {
+                case PuzzleSolvingMode.Part1:
+                    SolvePuzzlePart1(filename);
+                    break;
+                case PuzzleSolvingMode.Part2:
+                    SolvePuzzlePart2(filename);
+                    break;
+            }
+        }
+
+        private static void SolvePuzzlePart1(string filename)
+        {
+            foreach (string line in File.ReadLines(filename))
+            {
+                var crabPositions = line.Split(",", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                var max = crabPositions.Max();
+                var minimumFuelCost= Enumerable.Range(0, max + 1)
+                    .Select(index => crabPositions.Sum(p => Math.Abs(p - index)))
+                    .Min();
+                
+                Console.WriteLine("How much fuel must they spend to align to that position?");
+                Console.WriteLine(minimumFuelCost);
+            }
+        }
+
+        private static void SolvePuzzlePart2(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static PuzzleSolvingMode ParsePuzzleSolvingMode(string puzzlePartNumber)
+        {
+            if (!int.TryParse(puzzlePartNumber, out int number))
+                throw new ArgumentException($"2nd argument only supports values in the range [1,2]. Received {puzzlePartNumber}", "args[1]");
+            
+            return number switch
+            {
+                1 => PuzzleSolvingMode.Part1,
+                2 => PuzzleSolvingMode.Part2,
+                _ => throw new ArgumentException($"2nd argument only supports values in the range [1,2]. Received {number}", "args[1]")
+            };
+        }
+    }
+}
---
+++ AdventOfCode2021/Day7/AdventOfCode2021_Day7/PuzzleSolvingMode.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2021_Day7
+{
+    public enum PuzzleSolvingMode
+    {
+        Part1,
+        Part2
+    }
+}
